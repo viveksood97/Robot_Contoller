66b810a0a46a2d9316c4f350b7599777f0576842
vivek <vsood@umd.edu>
2021-10-05 16:22:50 -0400

    Created all CMakeLists, dummy main.cpp files and .gitignore file

---
+++ test/main.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file main.cpp
+ * @authors Vivek Sood and Charu Sharma
+ * @brief primary source file
+ * @date 2021-10-05
+ * @copyright Copyright (c) 2021
+ *
+ * ENPM808X Midterm - Phase 0 Proposal
+ *
+ */
