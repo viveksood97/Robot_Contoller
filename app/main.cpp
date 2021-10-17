976b315f1fce94ae64a8b48fdf59a0f8b4499b2b
vivek <vsood@umd.edu>
2021-10-17 03:01:12 -0400

    Added code to check if files are linked
    - Checking the linking of twoWDRobot.cpp, twoWDRobot.hpp by using TwoWDRobot class in
    main.cpp
    - Called computeHeading() method to check if it returns a value.
    - Called computeVelocity() method to check if it returns a value.
    - Need to remove this code eventually.

--- app/main.cpp
+++ app/main.cpp
@@ -11,12 +11,17 @@
 #include <iostream>
 #include "pid.hpp"
 #include "ackermann.hpp"
+#include "twoWDRobot.hpp"
 
 int main() {
     PID pid;
     Ackermann ackermann;
+    TwoWDRobot robot;
+
     std::cout << ackermann.computeModelOutputs(5) << std::endl;
     std::cout << pid.computePID(5) << std::endl;
+    std::cout << "Heading: " << robot.computeHeading(10, 10) << "\n"
+    << "Velocity: " << robot.computeVelocity(10, 10) << std::endl;
     return 0;
 }
 
