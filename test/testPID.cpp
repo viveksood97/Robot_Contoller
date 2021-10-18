521d5b53e0ae3078905a795d1a74990fa06275fa
vivek <vsood@umd.edu>
2021-10-17 20:30:08 -0400

    Added tests for all methods for PID class

--- test/testPID.cpp
+++ test/testPID.cpp
@@ -10,3 +10,20 @@
  */
 
 #include <gtest/gtest.h>
+#include "pid.hpp"
+
+TEST(PID, testSetAttributes) {
+    PID pid;
+    EXPECT_TRUE(pid.setKp(1.2));
+    EXPECT_TRUE(pid.setKi(1.2));
+    EXPECT_TRUE(pid.setKd(1.2));
+    EXPECT_TRUE(pid.setDt(0.1));
+    EXPECT_TRUE(pid.setTargetVelocity(1.2));
+}
+
+TEST(PID, testComputePID) {
+    PID pid;
+    double expectedResult = 6.9;
+    EXPECT_TRUE((pid.computePID(10) - expectedResult >= -0.1)
+    && (pid.computePID(10) - expectedResult <= 0.1));
+}
