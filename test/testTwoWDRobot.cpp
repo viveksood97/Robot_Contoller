0dd676822470ff5facfcd68ac791fba39058b3a9
vivek <vsood@umd.edu>
2021-10-17 21:12:02 -0400

    Added tests for all methods in TwoWDRobot class

--- test/testTwoWDRobot.cpp
+++ test/testTwoWDRobot.cpp
@@ -7,3 +7,24 @@
  * ENPM808X Midterm - Phase 0 Proposal
  *
  */
+
+#include <gtest/gtest.h>
+#include "twoWDRobot.hpp"
+
+TwoWDRobot twoWDRobot;
+
+TEST(TwoWDRobotTests, testSetAttributes) {
+    EXPECT_TRUE(twoWDRobot.setDt(0.1));
+}
+
+TEST(TwoWDRobotTests, testcomputeHeading) {
+    double expectedResult = 1.6;
+    EXPECT_TRUE((twoWDRobot.computeHeading(10, 10) - expectedResult >= -0.1)
+    && (twoWDRobot.computeHeading(10, 10) - expectedResult <= 0.1));
+}
+
+TEST(TwoWDRobotTests, testcomputeVelocity) {
+    double expectedResult = 1.7;
+    EXPECT_TRUE((twoWDRobot.computeVelocity(10, 10) - expectedResult >= -0.1)
+    && (twoWDRobot.computeVelocity(10, 10) - expectedResult <= 0.1));
+}
