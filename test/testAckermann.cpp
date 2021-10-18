a05249e05aee0991305eb71c9df0c3d833b33f8c
vivek <vsood@umd.edu>
2021-10-17 20:55:21 -0400

    Added tests for all methods for Ackermann Class

--- test/testAckermann.cpp
+++ test/testAckermann.cpp
@@ -7,3 +7,22 @@
  * ENPM808X Midterm - Phase 0 Proposal
  *
  */
+
+#include <gtest/gtest.h>
+#include "ackermann.hpp"
+
+Ackermann ackermann;
+
+TEST(AckermannTests, testSetAttributes) {
+    Ackermann ackermann;
+    EXPECT_TRUE(ackermann.setRobotProps(1.2, 1.2, 1.2, 1.2));
+    EXPECT_TRUE(ackermann.setDt(0.1));
+    EXPECT_TRUE(ackermann.setTargetHeading(1.2));
+}
+
+TEST(AckermannTests, testComputeModelOutputs) {
+    double expectedResult = 4.20;
+    EXPECT_TRUE((ackermann.computeModelOutputs(10) - expectedResult >= -0.1)
+    && (ackermann.computeModelOutputs(10) - expectedResult <= 0.1));
+}
+
