936bc6dcf932f8b728470867a867f9b6aadcdce2
Charu <charu107@umd.edu>
2021-10-25 06:13:24 -0400

    remove ackermann from main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -2,10 +2,9 @@
  * @file main.cpp
  * @authors Vivek Sood and Charu Sharma
  * @brief main file for testing
- * @date 2021-10-16
  * @copyright Copyright (c) 2021
  *
- * ENPM808X Midterm - Phase 1
+ * ENPM808X Midterm - Phase 2
  *
  */
 
@@ -15,12 +14,12 @@
 #include "twoWDRobot.hpp"
 
 int main() {
-    PID pid;
-    Ackermann ackermann;
+    // PID pid;
+    // Ackermann ackermann;
     TwoWDRobot robot;
 
-    std::cout << ackermann.computeModelOutputs(5) << std::endl;
-    std::cout << pid.computePID(5) << std::endl;
+    // std::cout << ackermann.computeModelOutputs(5) << std::endl;
+    // std::cout << pid.computePID(5) << std::endl;
     std::cout << "Heading: " << robot.computeHeading(10, 10) << "\n"
     << "Velocity: " << robot.computeVelocity(10, 10) << std::endl;
     return 0;
