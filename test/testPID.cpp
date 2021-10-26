02eed5d06c16ca60af33d1902b20292c3dfeffe4
Charu <charu107@umd.edu>
2021-10-25 20:53:44 -0400

    CommitID:2.13.2 Implemented PID tests - Created tests for all setters - Create convergence test for computePID() method

--- test/testPID.cpp
+++ test/testPID.cpp
@@ -1,29 +1,42 @@
-// /**
-//  * @file testPID.cpp
-//  * @authors Vivek Sood and Charu Sharma
-//  * @brief main file for testing
-//  * @date 2021-10-16
-//  * @copyright Copyright (c) 2021
-//  *
-//  * ENPM808X Midterm - Phase 1
-//  *
-//  */
+/**
+ * @file testPID.cpp
+ * @authors Vivek Sood and Charu Sharma
+ * @brief main file for testing
+ * @date 2021-10-16
+ * @copyright Copyright (c) 2021
+ *
+ * ENPM808X Midterm - Phase 2
+ *
+ */
 
-// #include <gtest/gtest.h>
-// #include "pid.hpp"
+#include <gtest/gtest.h>
+#include "pid.hpp"
 
-// PID pid;
+PID pid;
 
-// TEST(PID, testSetAttributes) {
-//     EXPECT_TRUE(pid.setKp(1.2));
-//     EXPECT_TRUE(pid.setKi(1.2));
-//     EXPECT_TRUE(pid.setKd(1.2));
-//     EXPECT_TRUE(pid.setDt(0.1));
-//     EXPECT_TRUE(pid.setTargetVelocity(1.2));
-// }
+TEST(PID, testSetAttributes) {
+    EXPECT_TRUE(pid.setKp(0.5));
+    EXPECT_TRUE(pid.setKi(0.005));
+    EXPECT_TRUE(pid.setKd(0.001));
+    EXPECT_TRUE(pid.setTargetVelocity(20));
+}
 
-// TEST(PID, testComputePID) {
-//     double expectedResult = 6.9;
-//     EXPECT_TRUE((pid.computePID(10) - expectedResult >= -0.1)
-//     && (pid.computePID(10) - expectedResult <= 0.1));
-// }
+TEST(PID, testComputePID) {
+    double testCurrentVelocity = 0;
+    clock_t startTime = clock();
+
+    double velocityDiff = 1000000;
+
+    while (true) {
+       clock_t endTime = clock();
+        double dt = (static_cast<double>
+        (endTime - startTime)) / CLOCKS_PER_SEC;
+
+        if (velocityDiff > 0.01) {
+            testCurrentVelocity = pid.computePID(testCurrentVelocity, dt);
+        } else {
+            break;
+        }
+    }
+    EXPECT_TRUE(testCurrentVelocity < 0.1);
+}
