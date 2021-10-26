853e01158c7020aeaf2a3292bea478f54f1518c3
Charu <charu107@umd.edu>
2021-10-25 22:05:02 -0400

    CommitID:2.13.4 Implemented tests for visualization library - Implemented tests for setters - Implemented test for printOutput method - Implemented tests for both plotting methods

--- test/testVisualization.cpp
+++ test/testVisualization.cpp
@@ -1,34 +1,45 @@
-// /**
-//  * @file testVisualization.cpp
-//  * @authors Vivek Sood and Charu Sharma
-//  * @date 2021-10-18
-//  * @copyright Copyright (c) 2021
-//  *
-//  * ENPM808X Midterm - Phase 1
-//  *
-//  */
+/**
+ * @file testVisualization.cpp
+ * @authors Vivek Sood and Charu Sharma
+ * @date 2021-10-18
+ * @copyright Copyright (c) 2021
+ *
+ * ENPM808X Midterm - Phase 1
+ *
+ */
 
-// #include <gtest/gtest.h>
-// #include <vector>
-// #include "visualization.hpp"
+#include <iostream>
+#include <gtest/gtest.h>
+#include "visualization.hpp"
 
-// Visualization visualization;
 
-// TEST(visualizationTests, testSetAttributes) {
-//     std::vector<double> testing{1, 2, 3, 4};
-//     EXPECT_TRUE(visualization.setVelocities(testing));
-//     EXPECT_TRUE(visualization.setHeadings(testing));
-//     EXPECT_TRUE(visualization.setTime(testing));
-// }
+Visualization visualization;
 
-// TEST(visualizationTests, testPlotVelocities) {
-//     std::vector<double> velocity{1, 2, 3, 4};
-//     std::vector<double> time{1, 2, 3, 4};
-//     EXPECT_TRUE(visualization.plotVelocities(velocity, time));
-// }
+TEST(visualizationTests, testSetAttributes) {
+    std::vector<double> testing{1, 2, 3, 4};
+    EXPECT_TRUE(visualization.setTargetVelocity(10));
+    EXPECT_TRUE(visualization.setTargetHeading(10));
+}
 
-// TEST(visualizationTests, testcomputeHeading) {
-//     std::vector<double> heading{1, 2, 3, 4};
-//     std::vector<double> time{1, 2, 3, 4};
-//     EXPECT_TRUE(visualization.plotHeadings(heading, time));
-// }
+TEST(visualizationTests, testPrintOutputs) {
+    std::cout << std::endl;
+    EXPECT_TRUE(visualization.printOutputs(10, 10));
+}
+
+TEST(visualizationTests, testPlotVelocities) {
+    std::vector<std::pair<double, double>> velocities;
+    for (int i = 0 ; i != 5; ++i) {
+       velocities.push_back(std::make_pair
+       (static_cast<double>(i), static_cast<double>(i)));
+    }
+    EXPECT_TRUE(visualization.plotVelocities(velocities));
+}
+
+TEST(visualizationTests, testPlotHeadings) {
+    std::vector<std::pair<double, double>> headings;
+    for (int i = 0 ; i != 5; ++i) {
+       headings.push_back(std::make_pair
+       (static_cast<double>(i), static_cast<double>(i)));
+    }
+    EXPECT_TRUE(visualization.plotHeadings(headings));
+}
